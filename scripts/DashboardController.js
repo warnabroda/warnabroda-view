95f179d54221899bbe6cb257688d3c132866f24e
Herbert Silva <hbt.vieira@gmail.com>
2015-01-27 23:35:58 -0200

    criada a modal de detalhes dos warnings

--- scripts/DashboardController.js
+++ scripts/DashboardController.js
@@ -8,11 +8,27 @@ warnabrodaApp.controller('DashboardController', ['$scope', '$rootScope', 'Dashbo
     	$scope.warn = {}
     	var warnings = {}
     	$scope.warning = {}
+    	$scope.ip = {}
 
     	$scope.searchFilter = {}
     	
-	   	var countAllWarnings = DashboardService.countAllWarnings();    		    
+	   	var countAllWarnings = DashboardService.countAllWarnings();  
 
+	   	$scope.ipDetails = function (ip){
+	   		
+	   		if (ip && !angular.isUndefined(ip)){
+	    		$.get("http://ipinfo.io/" + $scope.getIp(ip) + "/json", function(response) {
+		    		
+		    		$timeout(function() {
+				     	$scope.ip = response;
+					}, 100);
+				 }, "jsonp");
+			}
+	   	}
+
+	   	$scope.googleMapString = function (str){
+	   		return "https://maps.googleapis.com/maps/api/staticmap?center="+str+"&zoom=13&size=500x400&sensor=true&maptype=hybrid&markers=color:blue%7Clabel:S%7C"+str;
+	   	}
 
 	    countAllWarnings.then(function(result) {
 	    	if (result) {
@@ -38,23 +54,15 @@ warnabrodaApp.controller('DashboardController', ['$scope', '$rootScope', 'Dashbo
 		};
 
 		$scope.formatDate = function(date){
-			return new Date(date).toISOString();
-		}
-
-		$scope.formatPhone = function(value){
-
-			if (value.indexOf("@") < 0){				
-				return $filter('phonenumber')(value);
+			
+			if (date && !angular.isUndefined(date) && date != '0000-00-00 00:00:00'){
+				return new Date(date).toISOString();
 			}
-
-			return value;
-
-		}
+		}	
 
 		$scope.doSearch = function (sFilter){
 		    var allWarns = DashboardService.getAll(sFilter);
-		    allWarns.then(function(data){	   
-		    	
+		    allWarns.then(function(data){	   		    	
 				$scope.list = data;
 				$scope.currentPage = 1; //current page
 				$scope.entryLimit = 10; //max no of items to display in a page
@@ -69,6 +77,7 @@ warnabrodaApp.controller('DashboardController', ['$scope', '$rootScope', 'Dashbo
 
 			var w = DashboardService.get(id);
 			w.then(function(data){
+				$scope.ipDetails(data.ip);
 				$scope.warning = data;
 				$scope.selectedMessage = m;
 				$scope.selectedContactType = ct;
@@ -84,6 +93,27 @@ warnabrodaApp.controller('DashboardController', ['$scope', '$rootScope', 'Dashbo
             $('#dataModal').modal('hide');	
         }
 
+        $scope.getIp = function(strIp){
+        	if (strIp.indexOf(",") > -1){
+        		var ips = strIp.split(",");
+        		return ips[1];
+        	}
+
+        	return strIp;
+
+        }
+
+        $scope.formatReturnMessage = function(w){
+        	console.log(w);
+        	if (w.id_contact_type === 1){
+        		var formatedMessage = w.message.replace(","," , ");
+        		formatedMessage 	= formatedMessage.replace("{","{ ");
+        		formatedMessage 	= formatedMessage.replace("}"," }");
+        		return formatedMessage;
+        	}
+
+        	return w.message
+        }
+
 
     }]);
-
