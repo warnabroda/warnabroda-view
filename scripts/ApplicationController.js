a8250804a93848a5a1d6767bb9cd732a6db0a23f
Herbert Smith <hbt.vieira@gmail.com>
2015-06-29 01:16:07 -0300

    bugfix do scroll nos componentes

--- scripts/ApplicationController.js
+++ scripts/ApplicationController.js
@@ -2,11 +2,13 @@
 
 /* Controllers */
 
-warnabrodaApp.controller('ApplicationController', ['$scope', '$window', '$location', 'USER_ROLES', 'AuthenticationSharedService',
-    function ($scope, $window, $location, USER_ROLES, AuthenticationSharedService){
+warnabrodaApp.controller('ApplicationController', ['$scope', '$window', '$location', 'USER_ROLES', 'AuthenticationSharedService', 'deviceDetector', 
+    function ($scope, $window, $location, USER_ROLES, AuthenticationSharedService, deviceDetector){
     	$scope.currentUser = null;
 	  	$scope.userRoles = USER_ROLES;
 	  	$scope.isAuthorized = AuthenticationSharedService.isAuthorized;
+	  	$scope.deviceDetectorTest = deviceDetector;
+	  	$scope.test = "Nao deu!";
 	 
 	  	$scope.setCurrentUser = function (user) {
 	    	$scope.currentUser = user;	    	
